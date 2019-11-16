c351deb923666681cdcb33e2e66e837d11a6dad2
Dennis Duan <gwduan@gmail.com>
2019-11-16 10:54:38 +0800

    BinarySearchFirstEqualValue BinarySearchLastEqualValue BinarySearchFirstGEValue BinarySearchLastLEValue

--- search/binary.go
+++ search/binary.go
@@ -17,3 +17,97 @@ func BinarySearch(data []int, num int, value int) int {
 
 	return -1
 }
+
+func BinarySearchFirstEqualValue(data []int, num int, value int) int {
+	low := 0
+	high := num - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+		if data[mid] > value {
+			high = mid - 1
+			continue
+		}
+
+		if data[mid] < value {
+			low = mid + 1
+			continue
+		}
+
+		if mid == 0 || data[mid-1] != value {
+			return mid
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1
+}
+
+func BinarySearchLastEqualValue(data []int, num int, value int) int {
+	low := 0
+	high := num - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+		if data[mid] > value {
+			high = mid - 1
+			continue
+		}
+
+		if data[mid] < value {
+			low = mid + 1
+			continue
+		}
+
+		if mid == num-1 || data[mid+1] != value {
+			return mid
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return -1
+}
+
+func BinarySearchFirstGEValue(data []int, num int, value int) int {
+	low := 0
+	high := num - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+		if data[mid] < value {
+			low = mid + 1
+			continue
+		}
+
+		if mid == 0 || data[mid-1] < value {
+			return mid
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1
+}
+
+func BinarySearchLastLEValue(data []int, num int, value int) int {
+	low := 0
+	high := num - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+		if data[mid] > value {
+			high = mid - 1
+			continue
+		}
+
+		if mid == num-1 || data[mid+1] > value {
+			return mid
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return -1
+}
--- search/search_test.go
+++ search/search_test.go
@@ -8,3 +8,31 @@ func TestBinarySearch(t *testing.T) {
 		t.Errorf("got %v, want %v", got, 4)
 	}
 }
+
+func TestBinarySearchFirstEqualValue(t *testing.T) {
+	data := []int{1, 3, 4, 5, 6, 8, 8, 8, 14, 20}
+	if got := BinarySearchFirstEqualValue(data, len(data), 8); got != 5 {
+		t.Errorf("got %v, want %v", got, 5)
+	}
+}
+
+func TestBinarySearchLastEqualValue(t *testing.T) {
+	data := []int{1, 3, 4, 5, 6, 7, 8, 8, 8, 20}
+	if got := BinarySearchLastEqualValue(data, len(data), 8); got != 8 {
+		t.Errorf("got %v, want %v", got, 8)
+	}
+}
+
+func TestBinarySearchFirstGEValue(t *testing.T) {
+	data := []int{1, 3, 4, 5, 6, 8, 8, 8, 14, 20}
+	if got := BinarySearchFirstGEValue(data, len(data), 7); got != 5 {
+		t.Errorf("got %v, want %v", got, 5)
+	}
+}
+
+func TestBinarySearchLastLEValue(t *testing.T) {
+	data := []int{1, 3, 4, 5, 6, 7, 7, 8, 9, 20}
+	if got := BinarySearchLastLEValue(data, len(data), 7); got != 6 {
+		t.Errorf("got %v, want %v", got, 6)
+	}
+}
