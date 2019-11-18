6a134c8b06ce7f4cc6908c0045bbdc28f8b230ee
Dennis Duan <gwduan@gmail.com>
2019-11-18 22:38:44 +0800

    Skiplist cont.

--- skiplist/skiplist.go
+++ skiplist/skiplist.go
@@ -1,27 +1,39 @@
 package skiplist
 
-import "math/rand"
+import (
+	"errors"
+	"math/rand"
+)
 
 const MAX_LEVEL = 16
 
-type element struct {
+var (
+	ErrDuplicate = errors.New("Duplicate")
+	ErrNotFound  = errors.New("Not Found")
+)
+
+type Element struct {
 	value    int
 	level    int
-	forwards []*element
+	forwards []*Element
 }
 
 type SkipList struct {
-	head   *element
+	head   *Element
 	level  int
 	length int
 }
 
-func newElement(value int, level int) *element {
-	return &element{
+func newElement(value int, level int) *Element {
+	return &Element{
 		value:    value,
 		level:    level,
-		forwards: make([]*element, level, level),
+		forwards: make([]*Element, level, level),
 	}
+}
+
+func (e *Element) Value() int {
+	return e.value
 }
 
 func NewSkipList() *SkipList {
@@ -32,30 +44,23 @@ func NewSkipList() *SkipList {
 	}
 }
 
-func (s *SkipList) Insert(value int) {
+func (s *SkipList) Level() int {
+	return s.level
+}
+
+func (s *SkipList) Length() int {
+	return s.length
+}
+
+func (s *SkipList) Insert(value int) error {
 	newLevel := randomLevel()
 	if newLevel > s.level {
 		s.level++
 		newLevel = s.level
 	}
-
 	newNode := newElement(value, newLevel)
 
-	pre := s.head
-	for i := s.level - 1; i >= 0; i-- {
-		for pre.forwards[i] != nil && pre.forwards[i].value < value {
-			pre = pre.forwards[i]
-		}
-		if newLevel > i {
-			newNode.forwards[i] = pre.forwards[i]
-			pre.forwards[i] = newNode
-		}
-	}
-
-	s.length++
-}
-
-func (s *SkipList) Find(value int) *element {
+	preNodes := make([]*Element, newLevel, newLevel)
 	pre := s.head
 	for i := s.level - 1; i >= 0; i-- {
 		for pre.forwards[i] != nil && pre.forwards[i].value < value {
@@ -63,14 +68,25 @@ func (s *SkipList) Find(value int) *element {
 		}
 
 		if pre.forwards[i] != nil && pre.forwards[i].value == value {
-			return pre.forwards[i]
+			return ErrDuplicate
+		}
+
+		if newLevel > i {
+			preNodes[i] = pre
 		}
 	}
+
+	for i := 0; i < newLevel; i++ {
+		newNode.forwards[i] = preNodes[i].forwards[i]
+		preNodes[i].forwards[i] = newNode
+	}
+
+	s.length++
 
 	return nil
 }
 
-func (s *SkipList) Delete(value int) {
+func (s *SkipList) Delete(value int) error {
 	found := false
 	pre := s.head
 	for i := s.level - 1; i >= 0; i-- {
@@ -89,7 +105,63 @@ func (s *SkipList) Delete(value int) {
 
 	if found {
 		s.length--
+		return nil
 	}
+
+	return ErrNotFound
+}
+
+func (s *SkipList) Find(value int) (*Element, error) {
+	pre := s.head
+	for i := s.level - 1; i >= 0; i-- {
+		for pre.forwards[i] != nil && pre.forwards[i].value < value {
+			pre = pre.forwards[i]
+		}
+
+		if pre.forwards[i] != nil && pre.forwards[i].value == value {
+			return pre.forwards[i], nil
+		}
+	}
+
+	return nil, ErrNotFound
+}
+
+func (s *SkipList) FindAll() ([]*Element, error) {
+	if s.length == 0 {
+		return nil, ErrNotFound
+	}
+
+	results := make([]*Element, 0, s.length)
+	for pre := s.head; pre.forwards[0] != nil; pre = pre.forwards[0] {
+		results = append(results, pre.forwards[0])
+	}
+
+	return results, nil
+}
+
+func (s *SkipList) FindBetween(begin int, end int) ([]*Element, error) {
+	if s.length == 0 {
+		return nil, ErrNotFound
+	}
+
+	pre := s.head
+	for i := s.level - 1; i >= 0; i-- {
+		for pre.forwards[i] != nil && pre.forwards[i].value < begin {
+			pre = pre.forwards[i]
+		}
+	}
+
+	results := make([]*Element, 0, 100)
+	for pre.forwards[0] != nil && pre.forwards[0].value < end {
+		results = append(results, pre.forwards[0])
+		pre = pre.forwards[0]
+	}
+
+	if len(results) == 0 {
+		return nil, ErrNotFound
+	}
+
+	return results, nil
 }
 
 func randomLevel() int {
--- skiplist/skiplist_test.go
+++ skiplist/skiplist_test.go
@@ -1,42 +1,238 @@
 package skiplist
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestSkipList(t *testing.T) {
+func TestNewSkipList(t *testing.T) {
 	s := NewSkipList()
-	if got := s.Find(3); got != nil {
-		t.Errorf("find(3) = %v, want %v", got, nil)
+	if got := s.Level(); got != 0 {
+		t.Errorf("Level() = %v, want %v", got, 0)
+	}
+	if got := s.Length(); got != 0 {
+		t.Errorf("Length() = %v, want %v", got, 0)
+	}
+	if got, err := s.Find(3); err == nil {
+		t.Errorf("Find(3) = (%v, %v), want (%v, %v)", got, err, nil, ErrNotFound)
+	}
+	if err := s.Delete(5); err == nil {
+		t.Errorf("Delete(5) = %v, want %v", err, ErrNotFound)
+	}
+}
+
+func TestInsertOneValue(t *testing.T) {
+	s := NewSkipList()
+	if err := s.Insert(5); err != nil {
+		t.Errorf("Insert(5) = %v, want %v", err, nil)
+	}
+	if got := s.Level(); got != 1 {
+		t.Errorf("Level() = %v, want %v", got, 1)
+	}
+	if got := s.Length(); got != 1 {
+		t.Errorf("Length() = %v, want %v", got, 1)
+	}
+	if got, err := s.Find(3); err == nil {
+		t.Errorf("Find(3) = (%v, %v), want (%v, %v)", got, err, nil, ErrNotFound)
+	}
+	if got, err := s.Find(5); got.Value() != 5 || err != nil {
+		t.Errorf("Find(5) = (%v, %v), want (%v, %v)", got, err, 5, nil)
+	}
+	if err := s.Insert(5); err == nil {
+		t.Errorf("Insert(5) = %v, want %v", err, ErrDuplicate)
+	}
+	if err := s.Delete(1); err == nil {
+		t.Errorf("Delete(1) = %v, want %v", err, ErrNotFound)
+	}
+	if err := s.Delete(5); err != nil {
+		t.Errorf("Delete(5) = %v, want %v", err, nil)
+	}
+	if got := s.Level(); got != 0 {
+		t.Errorf("Level() = %v, want %v", got, 0)
+	}
+	if got := s.Length(); got != 0 {
+		t.Errorf("Length() = %v, want %v", got, 0)
+	}
+}
+
+func TestInsertMultiValues(t *testing.T) {
+	s := NewSkipList()
+	for i := 5; i <= 100; i += 5 {
+		if err := s.Insert(i); err != nil {
+			t.Errorf("Insert(%v) = %v, want %v", i, err, nil)
+		}
+	}
+	if got := s.Length(); got != 20 {
+		t.Errorf("Length() = %v, want %v", got, 20)
+	}
+	for i := 100; i > 0; i -= 4 {
+		if err := s.Insert(i); i%(4*5) != 0 && err != nil {
+			t.Errorf("Insert(%v) = %v, want %v", i, err, nil)
+		} else if i%(4*5) == 0 && err == nil {
+			t.Errorf("Insert(%v) = %v, want %v", i, err, ErrDuplicate)
+		}
+	}
+	if got := s.Length(); got != 40 {
+		t.Errorf("Length() = %v, want %v", got, 40)
+	}
+	if got, err := s.Find(40); got.Value() != 40 || err != nil {
+		t.Errorf("Find(40) = (%v, %v), want (%v, %v)", got, err, 40, nil)
+	}
+	if got, err := s.Find(33); err == nil {
+		t.Errorf("Find(33) = (%v, %v), want (%v, %v)", got, err, nil, ErrNotFound)
 	}
 
-	s.Insert(1)
-	s.Insert(5)
-	s.Insert(3)
-	if got := s.Find(3); got == nil || got.value != 3 {
-		t.Errorf("find(3) = %v, want %v", got, 3)
+	for i := 4; i <= 100; i += 4 {
+		s.Delete(i)
 	}
-	if got := s.Find(6); got != nil {
-		t.Errorf("find(6) = %v, want %v", got, nil)
+	if got := s.Length(); got != 15 {
+		t.Errorf("Length() = %v, want %v", got, 15)
+	}
+	if got, err := s.Find(36); err == nil {
+		t.Errorf("Find(36) = (%v, %v), want (%v, %v)", got, err, nil, ErrNotFound)
+	}
+	if got, err := s.Find(25); got.Value() != 25 || err != nil {
+		t.Errorf("Find(25) = (%v, %v), want (%v, %v)", got, err, 25, nil)
+	}
+	for i := 5; i <= 100; i += 5 {
+		s.Delete(i)
+	}
+	if got := s.Length(); got != 0 {
+		t.Errorf("Length() = %v, want %v", got, 0)
+	}
+	if got := s.Level(); got != 0 {
+		t.Errorf("Level() = %v, want %v", got, 0)
+	}
+}
+
+func TestFindAll(t *testing.T) {
+	s := NewSkipList()
+	for i := 5; i <= 100; i += 5 {
+		s.Insert(i)
+	}
+	for i := 100; i > 0; i -= 4 {
+		s.Insert(i)
 	}
 
-	s.Insert(2)
-	s.Insert(8)
-	s.Insert(7)
-	s.Insert(9)
-	s.Insert(4)
-	s.Insert(6)
-	if got := s.Find(8); got == nil || got.value != 8 {
-		t.Errorf("find(8) = %v, want %v", got, 8)
-	}
-	if got := s.Find(10); got != nil {
-		t.Errorf("find(10) = %v, want %v", got, nil)
+	wantValues := make([]int, 0, s.Length())
+	for i := 1; i <= 100; i++ {
+		if i%4 == 0 || i%5 == 0 {
+			wantValues = append(wantValues, i)
+		}
 	}
 
-	s.Delete(16)
-	if got := s.Find(8); got == nil || got.value != 8 {
-		t.Errorf("find(8) = %v, want %v", got, 8)
+	gots, _ := s.FindAll()
+	gotValues := make([]int, 0, len(gots))
+	for _, v := range gots {
+		gotValues = append(gotValues, v.Value())
 	}
-	s.Delete(6)
-	if got := s.Find(6); got != nil {
-		t.Errorf("find(6) = %v, want %v", got, nil)
+
+	if len(wantValues) != len(gotValues) {
+		t.Errorf("FindAll() numbers = %v, want %v", len(gotValues), len(wantValues))
 	}
+	for i, v := range gotValues {
+		if v != wantValues[i] {
+			t.Errorf("FindAll() no. %v = %v, want %v", i, v, wantValues[i])
+		}
+	}
+}
+
+func TestFindBetween(t *testing.T) {
+	s := NewSkipList()
+	for i := 5; i <= 100; i += 5 {
+		s.Insert(i)
+	}
+	for i := 100; i > 0; i -= 4 {
+		s.Insert(i)
+	}
+
+	wantValues := make([]int, 0, s.Length())
+	for i := 64; i < 94; i++ {
+		if i%4 == 0 || i%5 == 0 {
+			wantValues = append(wantValues, i)
+		}
+	}
+
+	gots, _ := s.FindBetween(64, 94)
+	gotValues := make([]int, 0, len(gots))
+	for _, v := range gots {
+		gotValues = append(gotValues, v.Value())
+	}
+
+	if len(wantValues) != len(gotValues) {
+		t.Errorf("FindBetween(64, 94) numbers = %v, want %v", len(gotValues), len(wantValues))
+	}
+	for i, v := range gotValues {
+		if v != wantValues[i] {
+			t.Errorf("FindBetween(64, 94) no. %v = %v, want %v", i, v, wantValues[i])
+		}
+	}
+}
+
+func TestPrintInfo(t *testing.T) {
+	s := NewSkipList()
+	for i := 5; i <= 100; i += 5 {
+		s.Insert(i)
+	}
+	for i := 100; i > 0; i -= 4 {
+		s.Insert(i)
+	}
+
+	printAllValues(s)
+
+	findValuePath(s, 4)
+	findValuePath(s, 10)
+	findValuePath(s, 12)
+	findValuePath(s, 16)
+	findValuePath(s, 48)
+	findValuePath(s, 60)
+	findValuePath(s, 76)
+	findValuePath(s, 80)
+	findValuePath(s, 100)
+
+	findValuePath(s, 7)
+	findValuePath(s, 27)
+	findValuePath(s, 53)
+	findValuePath(s, 77)
+	findValuePath(s, 91)
+}
+
+func printAllValues(s *SkipList) {
+	if s.length == 0 {
+		return
+	}
+
+	fmt.Printf("SkipList content begin --------\n")
+	fmt.Printf("level=%v, length=%v\n", s.level, s.length)
+	for pre := s.head; pre.forwards[0] != nil; pre = pre.forwards[0] {
+		fmt.Printf("(%v,%v) ", pre.forwards[0].value, pre.forwards[0].level)
+	}
+	fmt.Println()
+	fmt.Printf("SkipList content end ----------\n")
+}
+
+func findValuePath(s *SkipList, value int) {
+	times := 0
+	fmt.Printf("Find %v path: ", value)
+	pre := s.head
+	for i := s.level - 1; i >= 0; i-- {
+		for pre.forwards[i] != nil && pre.forwards[i].value < value {
+			fmt.Printf("(%v,%v) -> ", pre.forwards[i].value, i+1)
+			times++
+			pre = pre.forwards[i]
+		}
+
+		if pre.forwards[i] != nil && pre.forwards[i].value > value {
+			fmt.Printf("(%v,%v) -> ", pre.forwards[i].value, i+1)
+			times++
+		}
+
+		if pre.forwards[i] != nil && pre.forwards[i].value == value {
+			times++
+			fmt.Printf("(%v,%v), times=%v.\n", pre.forwards[i].value, i+1, times)
+			return
+		}
+	}
+
+	fmt.Printf("not found, times=%v.\n", times)
 }
