ccf81f669e65c28063cde35b7932f5d06810083b
Dennis Duan <gwduan@gmail.com>
2024-11-09 13:47:46 +0800

    Heap sort refactor

--- sort/heap.go
+++ sort/heap.go
@@ -1,32 +1,32 @@
 package sort
 
-func HeapSort(data []int, num int) {
+func HeapSort[E any](data []E, num int, cmp func(E, E) int) {
 	if num <= 1 {
 		return
 	}
 
-	buildHeap(data, num)
+	buildHeap(data, num, cmp)
 
 	for k := num - 1; k > 0; k-- {
 		data[0], data[k] = data[k], data[0]
 
-		heapify(data, 0, k-1)
+		heapify(data, 0, k-1, cmp)
 	}
 }
 
-func buildHeap(data []int, num int) {
+func buildHeap[E any](data []E, num int, cmp func(E, E) int) {
 	for i := (num - 1) / 2; i >= 0; i-- {
-		heapify(data, i, num-1)
+		heapify(data, i, num-1, cmp)
 	}
 }
 
-func heapify(data []int, i int, n int) {
+func heapify[E any](data []E, i int, n int, cmp func(E, E) int) {
 	for {
 		maxPos := i
-		if i*2+1 <= n && data[i] < data[i*2+1] {
+		if i*2+1 <= n && cmp(data[i], data[i*2+1]) < 0 {
 			maxPos = i*2 + 1
 		}
-		if i*2+2 <= n && data[maxPos] < data[i*2+2] {
+		if i*2+2 <= n && cmp(data[maxPos], data[i*2+2]) < 0 {
 			maxPos = i*2 + 2
 		}
 		if maxPos == i {
