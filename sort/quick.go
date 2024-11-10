137a86b75c81017ed1401e4962698625f3c445a0
Dennis Duan <gwduan@gmail.com>
2024-11-10 09:21:38 +0800

    Quick sort refactor

--- sort/quick.go
+++ sort/quick.go
@@ -1,25 +1,25 @@
 package sort
 
-func QuickSort(data []int, num int) {
-	quickSort(data, 0, num-1)
+func QuickSort[E any](data []E, num int, cmp func(E, E) int) {
+	quickSort(data, 0, num-1, cmp)
 }
 
-func quickSort(data []int, low int, high int) {
+func quickSort[E any](data []E, low int, high int, cmp func(E, E) int) {
 	if low >= high {
 		return
 	}
 
-	pivot := partition(data, low, high)
+	pivot := partition(data, low, high, cmp)
 
-	quickSort(data, low, pivot-1)
-	quickSort(data, pivot+1, high)
+	quickSort(data, low, pivot-1, cmp)
+	quickSort(data, pivot+1, high, cmp)
 }
 
-func partition(data []int, low int, high int) int {
+func partition[E any](data []E, low int, high int, cmp func(E, E) int) int {
 	pivot := data[high]
 	i := low
 	for j := low; j < high; j++ {
-		if data[j] < pivot {
+		if cmp(data[j], pivot) < 0 {
 			data[i], data[j] = data[j], data[i]
 			i++
 		}
